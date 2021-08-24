925c719363dc3999f90d65156faa9bb29812c9e8
Sagarika Shinde <sagarika.shinde@morningstar.com>
2021-08-24 17:03:53 +0530

    First Commit

---
+++ src/com/bridgelabz/linkedlist/LinkedListRunner.java
@@ -0,0 +1,23 @@
+package com.bridgelabz.linkedlist;
+
+public class LinkedListRunner {
+	
+	public static void main(String[] args) {
+		MyNode<Integer> myThirdNode = new MyNode<Integer>(56);
+		MyNode<Integer> mySecondNode = new MyNode<Integer>(30);
+		MyNode<Integer> myFirstNode = new MyNode<Integer>(70);
+		
+		
+		
+		
+		
+		
+		
+		
+		//myFirstNode.setNext(mySecondNode);
+		//mySecondNode.setNext(myThirdNode);
+		
+		//boolean result = myFirstNode.getNext().equals(mySecondNode) && mySecondNode.getNext().equals(myThirdNode);
+		//System.out.println(result);
+	}
+}
---
+++ src/com/bridgelabz/linkedlist/MyLinkedList.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.linkedlist;
+
+public class MyLinkedList {
+
+	private INode head;
+	
+	public MyLinkedList(){
+		this.head=null;
+	}
+}
